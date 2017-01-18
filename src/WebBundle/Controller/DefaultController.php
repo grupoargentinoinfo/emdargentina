e2a1ac00132c4c89e02d5a85d8f8d34b2580afcf
grupoargentinoinfo <grupoargentinoinfo@gmail.com>
2017-01-18 15:59:01 -0300

    Estructura muy báscia twig y html
    Plantilla base.html.twig
    Y extensión de ella en index y en prestadores-medicos.
    Controladores en WebBundle.

--- src/WebBundle/Controller/DefaultController.php
+++ src/WebBundle/Controller/DefaultController.php
@@ -10,4 +10,9 @@ class DefaultController extends Controller
     {
         return $this->render('WebBundle:Default:index.html.twig');
     }
+
+    public function prestadores_medicosAction()
+    {
+        return $this->render('WebBundle:Default:servicios.html.twig');
+    }
 }
